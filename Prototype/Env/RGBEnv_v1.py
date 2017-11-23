81e34b271ea9299af2490258d8143c5b3c65e83c
Huang <lhuang28@swarmctrl-12lin.ee.e.uh.edu>
2017-11-23 16:24:59 -0600

    update

--- Prototype/Env/RGBEnv_v1.py
+++ Prototype/Env/RGBEnv_v1.py
@@ -1,52 +1,62 @@
-import numpy as np, random, sys, os, time, matplotlib.pyplot as plt
 
-map_data_dir = os.path.abspath('./MapData')
+import numpy as np, random, sys, matplotlib.pyplot as plt, time, os
+
 random.seed(10)
+map_data_dir =os.path.abspath('./MapData')
 
 class MazeEnv():
     def __init__(self):
-        global mazeData, costData, mazeHeight, mazeWidth, robot_marker, goal_range
-        goal_range = 15
-        self.action_space = ['u', 'd', 'l', 'r']
+        global mazeData, costData, centerline, freespace, mazeHeight, mazeWidth, robot_marker, goal_range
         robot_marker = 150
+        goal_range = 10
+        self.action_space = ['u', 'd', 'l', 'r']
         self.n_actions = len(self.action_space)
-        mazeData, costData = self._load_data(map_data_dir)
+        mazeData, costData, centerline, freespace = self._load_data(map_data_dir)
         mazeHeight, mazeWidth = mazeData.shape
+
         self.maze = np.ones((mazeHeight, mazeWidth))-mazeData
-        self.goal = np.array([73,10])
+        self.centerline = np.ones((mazeHeight, mazeWidth))-centerline
+        self.freespace = np.ones((mazeHeight, mazeWidth))-freespace
+        self.goal = np.array([73, 10])
         self.init_state = []
         self._build_robot()
 
     def _load_data(self, data_directory):
-        mazeData = np.loadtxt(data_directory + '/scaled_maze7_freespace.csv').astype(int)
+        mazeData = np.loadtxt(data_directory + '/scaled_maze7.csv').astype(int)
+        freespace = np.loadtxt(data_directory + '/scaled_maze7_freespace.csv').astype(int)
         costData = np.loadtxt(data_directory + '/scaled_maze7_costmap.csv').astype(int)
-        return mazeData, costData
-
+        centerline = np.loadtxt(data_directory + '/scaled_maze7_centerline.csv').astype(int)
+        return mazeData, costData, centerline, freespace
 
     def _build_robot(self):
-        row, col = np.nonzero(mazeData)
+        row, col = np.nonzero(freespace)
+
         if not len(self.init_state):
-            self.robot_num = 5
+            self.robot_num = 10
             self.robot = random.sample(range(row.shape[0]), self.robot_num)
             self.state = np.zeros(np.shape(mazeData)).astype(int)
+            self.state_img = np.copy(self.state)
             for i in range(self.robot_num):
-               self.state[row[self.robot[i]], col[self.robot[i]]] = robot_marker
-                #self.state[row[i], col[i]]= 150
+                self.state[row[self.robot[i]], col[self.robot[i]]] += robot_marker
+                self.state_img[row[self.robot[i]]-1:row[self.robot[i]]+2,
+                    col[self.robot[i]]-1:col[self.robot[i]]+2] = robot_marker*np.ones([3,3])
 
             self.init_state = self.state
+            self.init_state_img = self.state_img
         else:
             self.state = self.init_state
+            self.state_img = self.init_state_img
 
-        output_img = self.state + 255*self.maze
 
+        output_img = self.state_img + self.maze * 255
 
         return output_img
 
     def step(self,action):
-        if action == 0:   # down
+        if action == 0:   # up
             next_direction = -1
             next_axis = 0
-        elif action == 1:   # up
+        elif action == 1:   # down
             next_direction = 1
             next_axis = 0
         elif action == 2:   # left
@@ -56,28 +66,34 @@ class MazeEnv():
             next_direction = 1
             next_axis = 1
 
-        # Translation motion is achieved by np.roll along 'next_axis' dimension, sign(next_direction) direction
-        # and abs(next_direction) steps
         next_state = np.roll(self.state, next_direction, axis=next_axis)
 
-        # Collision check: find robots in the obstacle area
-        collision = np.logical_and(next_state, self.maze)*next_state
+        # Collision check
+        collision = np.logical_and(next_state, self.freespace)*next_state
 
-        # Find robots in the free space
-        next_state = np.logical_xor(next_state, self.maze)*next_state
+        next_state *= np.logical_xor(next_state, self.freespace)
 
         # Move robots in the obstacle area back to previous grids and obtain the next state
         ## Case 1: overlapping with population index
         next_state += np.roll(collision, -next_direction, axis=next_axis)
         ## Case 2: overlapping w/o population index (0: no robot; 1: robot(s) exits)
-        #next_state = np.logical_or(np.roll(collision, -next_direction, axis=next_axis), next_state).astype(int)
+        # next_state = np.logical_or(np.roll(collision, -next_direction, axis=next_axis), next_state).astype(int)
 
         # next_state *= robot_marker   # Mark robot with intensity 150
 
+        row, col = np.nonzero(next_state)
+
+        self.state_img  = np.zeros([mazeHeight,mazeWidth])
+
+        for i in range(row.shape[0]):
+            self.state_img[row[i]-1:row[i]+2, col[i]-1:col[i]+2] = robot_marker * np.ones([3, 3])
+
         self.state = next_state
-        output_img = (self.state>0)*robot_marker + 255*self.maze
-        cost_to_go = np.sum(self.state*costData/robot_marker)
-        if cost_to_go <= goal_range*self.robot_num:
+
+        output_img = self.state_img + self.maze*255
+
+        cost_to_go = np.sum(self.state * costData / robot_marker)
+        if cost_to_go <= goal_range * self.robot_num:
             done = True
             reward = 100.0
         else:
@@ -87,14 +103,16 @@ class MazeEnv():
         return(output_img,reward,done,1)
 
     def render(self):
-        print '\n{}\n'.format(self.state)
+        plt.imshow(self.state_img + self.maze*255, vmin=0, vmax=255)
+        plt.show()
 
     def reset(self):
         return self._build_robot()
 
+
     def expert(self, robot_loc):
 
-        if len(robot_loc)==0:
+        if not len(robot_loc):
             return self.expert_restart_session()
 
         cost_to_goal = costData[robot_loc[0], robot_loc[1]]
@@ -123,33 +141,34 @@ class MazeEnv():
         else:
             return self.expert_restart_session()
 
-
-
     def expert_restart_session(self):
-        if (np.sum(self.state*costData/robot_marker)>goal_range):
+        if (np.sum(self.state*costData/robot_marker)>self.robot_num*goal_range):
             robot_loc = np.unravel_index(np.argmax((self.state > 0) * costData), self.state.shape)
         return self.expert(robot_loc)
 
-
-# #
-
-
-env = MazeEnv()
-env.reset()
-robot_loc = []
-for i in range(1000):
-    # next_action = np.random.randint(4,size = 1)
-    next_action, robot_loc = env.expert(robot_loc)
-
-    state, reward, done, _ = env.step(next_action)
-    if i%100 ==0:
-        print ' '
-    print 'step = {}, action = {}, reward = {}, done = {}'.format(i, next_action, reward, done )
-    if done:
-        break
-
-
-   # sys.stdout.flush()
-
+# env = MazeEnv()
+# env.render()
+# n_epochs = 1000
+# robot_loc =[]
+#
+# for i in range(n_epochs):
+#     #next_action = np.random.randint(4,size = 1)
+#     next_action, robot_loc = env.expert(robot_loc)
+#     state_img,reward, done, _ = env.step(next_action)
+#     print i, reward, done
+#     if i % 100 == 1:
+#         print ''
+#     #     plt.subplot( (n_epochs/100+1)/3+1,3, (i/100+1))
+#     #     plt.axis('off')
+#     #     plt.title('Step = ' + str(i) )
+#         env.render()
+#         plt.show()
+#     #     #plt.subplots_adjust(wspace = 0.1)
+#
+#     if done:
+#         env.render()
+#         plt.show()
+#         print i
+#         break
 
 
