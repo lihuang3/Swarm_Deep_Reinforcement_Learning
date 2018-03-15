f5b5f1bbc20711c2c678580fbb323305903374f3
Huang <lhuang28@swarmctrl-12lin.ee.e.uh.edu>
2018-03-15 18:54:20 -0500

    update

--- CA3C_beta/estimators.py
+++ CA3C_beta/estimators.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.rnn as rnn
 
-def cnn(X, add_summaries=False):
+def build_shared_network(X):
   """
   Builds a 3-layer network conv -> conv -> fc as described
   in the A3C paper. This network is shared by both the policy and value net.
@@ -16,78 +16,97 @@ def cnn(X, add_summaries=False):
   """
 
   # Three convolutional layers
-  conv1 = tf.contrib.layers.conv2d(
-    X, 16, 8, 4, activation_fn=tf.nn.relu, scope="conv1")
-  conv2 = tf.contrib.layers.conv2d(
-    conv1, 32, 4, 2, activation_fn=tf.nn.relu, scope="conv2")
+  # conv1 = tf.contrib.layers.conv2d(
+  #   X, 16, 8, 4, activation_fn=tf.nn.relu, scope="conv1")
+  # conv2 = tf.contrib.layers.conv2d(
+  #   conv1, 32, 4, 2, activation_fn=tf.nn.relu, scope="conv2")
+
+  # # Fully connected layer
+  # fc1 = tf.contrib.layers.fully_connected(
+  #   inputs=tf.contrib.layers.flatten(conv2),
+  #   num_outputs=256,
+  #   scope="fc1")
+
+  # Three convolutional layers
+  conv1 = tf.layers.conv2d(
+    input=X, filters=32, kernel_size=8, strides=4, activation=tf.nn.relu, scope="conv1")
+  conv2 = tf.layers.conv2d(
+    input=conv1, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, scope="conv2")
+  conv3 = tf.layers.conv2d(
+    input=conv2, filters=64, kernel_size=3, strides=1, activation=tf.nn.relu, scope="conv3")
 
   # Fully connected layer
-  fc1 = tf.contrib.layers.fully_connected(
-    inputs=tf.contrib.layers.flatten(conv2),
-    num_outputs=256,
-    scope="fc1")
+  fc1 = tf.layers.dense(
+    inputs=tf.layers.flatten(conv3), num_outputs=512, scope="fc1", activation=tf.nn.relu)
 
-  if add_summaries:
-    tf.contrib.layers.summarize_activation(conv1)
-    tf.contrib.layers.summarize_activation(conv2)
-    tf.contrib.layers.summarize_activation(fc1)
+  # if add_summaries:
+  #   tf.contrib.layers.summarize_activation(conv1)
+  #   tf.contrib.layers.summarize_activation(conv2)
+  #   tf.contrib.layers.summarize_activation(fc1)
 
   return fc1
 
-def cnn_lstm():
-    """
-    Builds a lstm rnn cnn_fc -> lstm -> feature space
+class cnn_lstm():
+  """
+  Builds a lstm rnn cnn_fc -> lstm -> feature space
+  and output policy logits and value fcn loss
 
-    Args:
+  Args:
+  """
 
-    """
+  def __init__(self, scope, feature_space, action_space):
+    self.state = X = tf.placeholder(shape=[None, 84, 84, 4], dtype=tf.uint8, name="X")
 
-    def __init__(self, feature_space, action_space):
-      self.state = X = tf.placeholder(shape=[None, 84, 84, 4], dtype=tf.uint8, name="X")
+    X = tf.to_float(X)/255.0
+    batch_size = tf.shape[X][0]
+    with tf.variable_scope(scope, reuse=True):
+       phi = build_shared_network(X)
 
-      batch_size = tf.shape[X][0]
+    # Xt is the time series input for LSTMs--> augment a fake batch dimension of 1 to
+    # do LSTMs over time dimension
+    Xt = tf.expand_dims(phi, [0])
 
-      # Three convolutional layers
-      conv1 = tf.layers.conv2d(
-          input=X, filters=32, kernel_size=8, strides=4, activation=tf.nn.relu, scope="conv1")
-      conv2 = tf.layers.conv2d(
-          input=conv1, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, scope="conv2")
-      conv3 = tf.layers.conv2d(
-          input=conv2, filters=64, kernel_size=3, strides=1, activation=tf.nn.relu, scope="conv3")
+    # Initialize RNN-LSTMs cell with feature space size = 256
+    lstm = rnn.BasicLSTMCell(num_units=feature_space, forget_bias=1.0, state_is_tuple=True)
+    # Todo: is this time sequence step size?
+    step_size = tf.shape(Xt)[:1]
 
-      # Fully connected layer
-      fc1 = tf.layers.dense(
-          inputs=tf.layers.flatten(conv3), num_outputs=512, scope="fc1",activation=tf.nn.relu)
+    # Reset lstm memeory cells
+    # lstm cell is a tuple = [c_in, h_in]
+    c_init = np.zeros((1, lstm.state_size.c), np.float32)
+    h_init = np.zeros((1, lstm.state_size.h), np.float32)
+    self.state_init = [c_init, h_init]
 
-      # Xt is the time series input for LSTMs--> augment a fake batch dimension of 1 to
-      # do LSTMs over time dimension
-      Xt = tf.expand_dims(fc1, [0])
+    # Pass lstm state from last training to the current training
+    c_in = tf.placeholder(tf.float32, [1, lstm.state_size.c], name='c_in')
+    h_in = tf.placeholder(tf.float32, [1, lstm.state_size.h], name='h_in')
+    self.state_in = [c_in, h_in]
 
-      # Initialize RNN-LSTMs cell with feature space size = 256
-      lstm = rnn.BasicLSTMCell(num_units=feature_space, forget_bias=1.0, state_is_tuple=True)
-      # Todo: is this time sequence step size?
-      step_size = tf.shape(Xt)[:1]
+    init_tuple = rnn.rnn_cell.LSTMStateTuple(c_in, h_in)
 
-      # Reset lstm memeory cells
-      # lstm cell is a tuple = [c_in, h_in]
-      c_init = np.zeros((1, lstm.state_size.c), np.float32)
-      h_init = np.zeros((1, lstm.state_size.h), np.float32)
-      self.state_init = [c_init, h_init]
+    lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
+        lstm, Xt, initial_state=init_tuple, sequence_length=batch_size, time_major=False)
 
-      # Pass lstm state from last training to the current training
-      c_in = tf.placeholder(tf.float32, [1, lstm.state_size.c], name='c_in')
-      h_in = tf.placeholder(tf.float32, [1, lstm.state_size.h], name='h_in')
+    lstm_c, lstm_h = lstm_state
 
-      init_tuple = rnn.rnn_cell.LSTMStateTuple(c_in, h_in)
+    # RNN feature-space state
+    psi = tf.reshape(lstm_outputs, [-1, feature_space])
+    self.state_out = [lstm_c[:1, :], lstm_h[:1, :]]
 
-      lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
-          lstm, Xt, initial_state=init_tuple, sequence_length=batch_size, time_major=False)
+    self.value_fcn = tf.layers.dense(inputs=psi, num_outputs=1, scope="value_fcn", activation=None)
 
-      lstm_c, lstm_h = lstm_state
+    self.policy_logits = tf.layers.dense(inputs=psi, num_outputs=action_space, scope="policy_fcn", activation=None)
+    self.policy = tf.nn.softmax(logits=self.policy_logits, dim=-1)[0,:]
+    self.action = tf.one_hot(tf.squeeze(input=tf.multinomial(logits=self.policy), axis=1), action_space)[0,:]
 
-      # RNN feature-space state
-      phi = tf.reshape(lstm_outputs, [-1, feature_space])
-      self.state_out = [lstm_c[:1, :], lstm_h[:1, :]]
+
+  def reset_lstm(self):
+    return self.state_init
+
+  def predictions(self, env_state, lstm_cin, lstm_hin):
+    sess = tf.get_default_session()
+    return sess.run([self.value_fcn, self.action],
+                    feed_dict={self.state: env_state, self.state_int[0]:lstm_cin, self.state_init[1]:lstm_hin})
 
 
 
